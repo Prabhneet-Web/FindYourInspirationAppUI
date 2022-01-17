9229a7cfa056058d11c37a5646a86f97a7a83ea0
Prabhneet Narula <imailcode@gmail.com>
2022-01-17 23:51:21 +0530

    Made the body scrollable.

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -26,109 +26,111 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      BorderRadius.vertical(bottom: Radius.circular(30))),
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Find Your",
-                      style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500)),
-                  const Text("Inspiration",
-                      style: TextStyle(
-                          color: Colors.black87,
-                          fontSize: 40,
-                          fontWeight: FontWeight.w600)),
-                  const SizedBox(height: 10),
-                  Container(
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        color: const Color.fromRGBO(244, 243, 243, 1),
-                        borderRadius: BorderRadius.circular(15)),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.search_rounded,
-                              color: Colors.black87, size: 25),
-                          hintText: "Search you're looking for",
-                          hintStyle:
-                              TextStyle(color: Colors.grey, fontSize: 15)),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(30))),
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Find Your",
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500)),
+                    const Text("Inspiration",
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 40,
+                            fontWeight: FontWeight.w600)),
+                    const SizedBox(height: 10),
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: const Color.fromRGBO(244, 243, 243, 1),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(Icons.search_rounded,
+                                color: Colors.black87, size: 25),
+                            hintText: "Search you're looking for",
+                            hintStyle:
+                                TextStyle(color: Colors.grey, fontSize: 15)),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                ],
+                    const SizedBox(height: 10),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Promo Today!",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 20),
-                  Container(
-                    height: 200,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        promoCard("lib/assets/images/Motivation1.png"),
-                        promoCard("lib/assets/images/Motivation2.jpeg"),
-                        promoCard("lib/assets/images/Motivation3.png"),
-                        promoCard("lib/assets/images/Motivation4.png")
-                      ],
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Promo Today!",
+                        style:
+                            TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          promoCard("lib/assets/images/Motivation1.png"),
+                          promoCard("lib/assets/images/Motivation2.jpeg"),
+                          promoCard("lib/assets/images/Motivation3.png"),
+                          promoCard("lib/assets/images/Motivation4.png")
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    height: 160,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage(
-                                "lib/assets/images/Motivation2.jpeg"))),
-                    child: Container(
+                    SizedBox(height: 20),
+                    Container(
+                      height: 160,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                          gradient: LinearGradient(
-                              begin: Alignment.bottomRight,
-                              stops: [
-                                0.3,
-                                0.9
-                              ],
-                              colors: [
-                                Colors.black.withOpacity(0.8),
-                                Colors.black.withOpacity(0.2)
-                              ])),
-                      child: Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Text(
-                            "Best Design",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(
+                                  "lib/assets/images/Motivation2.jpeg"))),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: LinearGradient(
+                                begin: Alignment.bottomRight,
+                                stops: [
+                                  0.3,
+                                  0.9
+                                ],
+                                colors: [
+                                  Colors.black.withOpacity(0.8),
+                                  Colors.black.withOpacity(0.2)
+                                ])),
+                        child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Text(
+                              "Best Design",
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
